920f3cad5786d242291607f7e949b6cd6efbee89
Alexandra Abrosimova <alexandra.abrosimova@teligent.ru>
2020-03-31 09:58:48 +0300

    book create page template

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import {Navbar} from './components/Navbar'
 import {Home} from './pages/Home'
-import {CreateBook} from './pages/CreateBook'
+import CreateBook from './pages/CreateBook'
 import {BookDetail} from './pages/BookDetail'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <div className="container-fluid pt-4">
-        <Switch>
-          <Route path="/create" component={CreateBook} />
-          <Route path="/books/:id" component={BookDetail} />
-          <Route path="/" exact component={Home} />
-        </Switch>
+      <div className="flex-grow-1 bg-light">
+        <Navbar />
+        <div className="container-fluid pt-5">
+          <Switch>
+            <Route path="/create" component={CreateBook} />
+            <Route path="/books/:id" component={BookDetail} />
+            <Route path="/" exact component={Home} />
+          </Switch>
+        </div>
       </div>
     </BrowserRouter>
   );
--- src/pages/CreateBook.js
+++ src/pages/CreateBook.js
@@ -1,7 +1,73 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { createControl } from '../form/formFramework'
 
-export const CreateBook = () => (
-  <div>
-    <h1>Create book</h1>
-  </div>
-)
+
+function createOptionControl(number) {
+  return createControl({
+    label: `Вариант ${number}`,
+    id: number,
+    errorMessage: 'Значение не может быть пустым'
+  }, {required: true});
+};
+
+function createFormControls() {
+  return {
+    question: createControl({
+      label: 'Введите вопрос',
+      errorMessage: 'Вопрос не может быть пустым'
+    }, {required: true}),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4)
+  };
+};
+
+class CreateBook extends Component {
+
+  state = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="mb-5">
+          <h1 className="text-center">Добавить книгу</h1>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-sm-12 col-md-10 col-lg-5">
+            <div className="shadow-sm bg-white rounded p-5">
+              <form>
+                <div className="form-group mb-4">
+                  <label htmlFor="bookAuthor" class="sr-only">Автор книги</label>
+                  <input type="text" className="form-control form-control-lg" id="bookAuthor" placeholder="Автор книги" />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="bookName" class="sr-only">Название</label>
+                  <input type="text" className="form-control form-control-lg" id="bookName" placeholder="Название" />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="bookYear" class="sr-only">Год выпуска книги</label>
+                  <input type="text" className="form-control form-control-lg" id="bookYear" placeholder="Год выпуска книги" />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="author" class="sr-only">Местоположение</label>
+                  <input type="text" className="form-control form-control-lg" id="author" placeholder="Местоположение" />
+                </div>
+                <div className="form-group mb-4">
+                  <label htmlFor="bookFace" class="">Фотография обложки</label>
+                  <input type="file" className="form-control-file" id="bookFace" />
+                </div>
+                <button type="submit" class="btn btn-primary">Сохранить</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default CreateBook
